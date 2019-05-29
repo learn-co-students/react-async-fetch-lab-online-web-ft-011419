d87ca8cf9bdaf989a32f8cc54f55b8ea57b45035
gbs4ever <gbs4ever2@gmail.com>
2019-05-29 14:51:33 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,36 @@
 // create your App component here
+import React, { Component } from 'react';
+
+class App extends Component {
+constructor(){
+super();
+
+this.state = {
+ astros:[]
+  }
+}
+  //fetch = () =>{
+
+  //}
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          astros: data.people
+        })
+      })
+  }
+   //{this.state.astros.map(person => person.name)}<br></br>
+
+      render() {
+        return (
+          <div>
+            {this.state.astros.map(person => person.name)}
+          </div>
+        )
+       }
+       
+
+}
+export default App;
